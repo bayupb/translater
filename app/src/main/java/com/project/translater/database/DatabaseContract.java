a7fba7bdb4864746a0f5711b660c8586fa32a01b
kid49 <bayupm124@gmail.com>
2021-05-11 23:29:35 +0700

    translater-indo

---
+++ app/src/main/java/com/project/translater/database/DatabaseContract.java
@@ -0,0 +1,15 @@
+package com.project.translater.database;
+import android.provider.BaseColumns;
+
+public class DatabaseContract {
+
+    static String TABLE_ENGLISH = "tb_inggris";
+    static String TABLE_INDONESIA = "tb_indonesia";
+
+    static final class KamusColumns implements BaseColumns {
+
+        static String KATA = "kata";
+        static String DESKRIPSI = "deskripsi";
+
+    }
+}
